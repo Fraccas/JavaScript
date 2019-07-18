25295d0c5dc2d04f0b7e248133e3e099c2c39592
Fraccas <daltonward1@yahoo.com>
2019-07-18 12:53:11 -0500

    99 lines of code lab

---
+++ linesofcode_lab.js
@@ -0,0 +1,10 @@
+
+let friends = ["D2", "Toast", "Van", "Dat Boi", "Duck"];
+
+for (let f of friends) {
+    for (var i = 99; i > 0; i--) {
+        console.log(i + " lines of code in the file, " + i + " lines of code; " + 
+        f + " strikes one out, clears it all out, " + i + " lines of code in the file");
+    }
+    console.log("--------------------------");
+}
